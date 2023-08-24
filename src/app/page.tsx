c4a57640f4de217a5dda6f4cb0ba33037ae7070c
Lucas Manoguerra <kiu.manoguerra@gmail.com>
2023-08-24 15:35:32 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { LeftBar } from '../components/left-bar/leftBar'
-import { SectionMessage } from '../components/section-message/section-message'
-
 export default function Home() {
-    return (
-        // <main className={styles.contentApp}>
-        //     <LeftBar />
-        //     <SectionMessage />
-        // </main>
-        <></>
-    )
+  return <></>
 }
